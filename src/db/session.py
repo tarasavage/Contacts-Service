bfff44c7cd0b05b0f178c46af67a7bdce48e060a
Taras Panasiuk <107722931+tarasavage@users.noreply.github.com>
2023-08-17 21:24:07 +0300

    Created backend for auth and added session.py, user_db_py factories

---
+++ src/db/session.py
@@ -0,0 +1,9 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.db.database import async_session_maker
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
